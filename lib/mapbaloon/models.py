4ec068ba6e43d850d49883a6e93b2c11ec1116ed
ruslan.galimov <ruslan.galimov@corp.mail.ru>
2015-10-08 00:57:00 +0200

    Fix tabs

--- lib/mapbaloon/models.py
+++ lib/mapbaloon/models.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 from django.db import models
 from django.utils import timezone
 from django.utils.http import urlquote
@@ -10,23 +11,23 @@ from lib.photo.models import Photo
 
 
 class Format(models.Model):
-	name = models.CharField(max_length=15)
+    name = models.CharField(max_length=15)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
-	def __str__(self):
+    def __str__(self):
         return self.name
 
 
 class Instrument(models.Model):
-	name = models.CharField(max_length=511)
+    name = models.CharField(max_length=511)
 
-	def __unicode__(self):
-		return self.name	
-	
-	def __str__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name    
+    
+    def __str__(self):
+        return self.name
 
 
 class GeoObject(models.Model):
@@ -38,47 +39,47 @@ class GeoObject(models.Model):
 
 
 class Balloon(models.Model):
-	def __unicode__(self):
-		return str(self.coord1) + ' ' + str(self.coord2)
-	
-	def __str__(self):
-		return str(self.coord1) + ' ' + str(self.coord2)
+    def __unicode__(self):
+        return str(self.coord1) + ' ' + str(self.coord2)
+    
+    def __str__(self):
+        return str(self.coord1) + ' ' + str(self.coord2)
 
-	coord1 = models.FloatField()
-	coord2 = models.FloatField()
-	date = models.DateField()
-	isugrshoot = models.BooleanField(default=False, blank=True) #having underground communication shooting
-	isaltmark = models.BooleanField(default=False, blank=True) #having altitude mark
-	isrelelems = models.BooleanField(default=False, blank=True) #having relief elements
+    coord1 = models.FloatField()
+    coord2 = models.FloatField()
+    date = models.DateField()
+    isugrshoot = models.BooleanField(default=False, blank=True) #having underground communication shooting
+    isaltmark = models.BooleanField(default=False, blank=True) #having altitude mark
+    isrelelems = models.BooleanField(default=False, blank=True) #having relief elements
 
-	publisher = models.ForeignKey(CustomUser)
-	myFormat = models.ForeignKey(Format)
+    publisher = models.ForeignKey(CustomUser)
+    myFormat = models.ForeignKey(Format)
 
-	syscoord = models.CharField(max_length=255)
-	sysaltit = models.CharField(max_length=255)	
-	
-	instrument = models.ForeignKey(Instrument)
+    syscoord = models.CharField(max_length=255)
+    sysaltit = models.CharField(max_length=255)	
+    
+    instrument = models.ForeignKey(Instrument)
 
 
 class Polygon(models.Model):
-	date = models.DateField()
-	isugrshoot = models.BooleanField(default=False, blank=True) #having underground communication shooting
-	isaltmark = models.BooleanField(default=False, blank=True) #having altitude mark
-	isrelelems = models.BooleanField(default=False, blank=True) #having relief elements
+    date = models.DateField()
+    isugrshoot = models.BooleanField(default=False, blank=True) #having underground communication shooting
+    isaltmark = models.BooleanField(default=False, blank=True) #having altitude mark
+    isrelelems = models.BooleanField(default=False, blank=True) #having relief elements
 
-	publisher = models.ForeignKey(CustomUser)
-	myFormat = models.ForeignKey(Format)
+    publisher = models.ForeignKey(CustomUser)
+    myFormat = models.ForeignKey(Format)
 
-	syscoord = models.CharField(max_length=255, default='None', blank=True)
-	sysaltit = models.CharField(max_length=255, default='None', blank=True)	
-	
-	instrument = models.ForeignKey(Instrument)
+    syscoord = models.CharField(max_length=255, default='None', blank=True)
+    sysaltit = models.CharField(max_length=255, default='None', blank=True)	
+    
+    instrument = models.ForeignKey(Instrument)
 
 
 class PolygonCoord(models.Model):
-	pgowner = models.ForeignKey(Polygon)
-	coord1 = models.FloatField()
-	coord2 = models.FloatField()
+    pgowner = models.ForeignKey(Polygon)
+    coord1 = models.FloatField()
+    coord2 = models.FloatField()
 
 
 class TriangulationStation(models.Model):
@@ -91,11 +92,11 @@ class TriangulationStation(models.Model):
     )
 
     title = models.CharField(max_length=255)
-    type = models.CharField(max_length-7, choices=TriangulationStation.TYPE_CHOICES)
+    type = models.CharField(max_length=7, choices=TYPE_CHOICES)
     precision = models.IntegerField()
     national = models.BooleanField()
     height = models.IntegerField()
-    backsight = models.BoolenField()
+    backsight = models.BooleanField()
     outer = models.BooleanField()
     center = models.BooleanField()
     center_height = models.BooleanField()
