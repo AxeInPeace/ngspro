ced9cba8010da1b711eb40470b9686db9d008140
Galimov Ruslan <delfin1995@yandex.ru>
2015-10-18 20:34:37 +0300

    Update views.py

--- lib/core/views.py
+++ lib/core/views.py
@@ -16,16 +16,3 @@ class JSONResponse(HttpResponse):
     def __init__(self, data):
         content = json.dumps(data, ensure_ascii=False)
         super(JSONResponse, self).__init__(content=content, content_type='application/json; charset=utf-8')
-
-
-
-
-
-
-
-
-
-
-
-
-
