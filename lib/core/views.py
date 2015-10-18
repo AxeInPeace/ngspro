b1f023d014febb6f8fb6e7ccd14228eec2e45bc5
ruslan.galimov <ruslan.galimov@corp.mail.ru>
2015-10-18 19:32:53 +0200

    Refactor

--- lib/core/views.py
+++ lib/core/views.py
@@ -1,10 +1,31 @@
 import json
+import uuid
 
+
+from django.conf import settings
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+from lib.photo.models import Photo
+from lib.core.utils import upload_file
 
 
 class JSONResponse(HttpResponse):
     def __init__(self, data):
         content = json.dumps(data, ensure_ascii=False)
         super(JSONResponse, self).__init__(content=content, content_type='application/json; charset=utf-8')
+
+
+
+
+
+
+
+
+
+
+
+
 
--- lib/photo/views.py
+++ lib/photo/views.py
@@ -8,31 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 from lib.core.views import JSONResponse
+from lib.core.utils import upload_file
 from lib.photo.models import Photo
-
-
-def list(request):
-    # Handle file upload
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            newdoc = Document(docfile = request.FILES['docfile'])
-            newdoc.save()
-
-            # Redirect to the document list after POST
-            return HttpResponseRedirect(reverse('myapp.views.list'))
-    else:
-        form = DocumentForm() # A empty, unbound form
-
-    # Load documents for the list page
-    documents = Document.objects.all()
-
-    # Render list page with the documents and the form
-    return render_to_response(
-        'myapp/list.html',
-        {'documents': documents, 'form': form},
-        context_instance=RequestContext(request)
-    )
 
 
 def get_filtered_name(name):
@@ -65,15 +42,15 @@ def rewrite_url(url):
 @csrf_exempt
 @require_http_methods(["POST"])
 def upload(request):
-    f = request.FILES['material']
-    client = storage.Client.from_service_account_json(settings.KEYFILE, settings.GCS_PROJECT)  # TODO: rel paths
-    bucket = client.get_bucket(settings.GCS_BUCKET)
-    name = get_filtered_name(f.name)
-    if name is None:
-        return JSONResponse({'status': 400, 'message': 'wrong file'})
-    blob = bucket.blob(name)
-    blob.upload_from_string(f.read())
-    blob.make_public()
-    Photo.objects.create(url=rewrite_url(blob.public_url))
+    f = request.FILES.get('material', None)
 
-    return JSONResponse({'status': 200, 'message': 'ok', 'url': rewrite_url(blob.public_url)})
+    if f is None:
+        return JSONResponse({'status': 400, 'message': 'No file'})
+    
+    url = upload_file(f, f.name, filter=['jpg', 'png', 'jpeg'])
+
+    if url is None:
+        return JSONResponse({'status': 400, 'message': 'wrong file'})
+    
+    Photo.objects.create(url=url)
+    return JSONResponse({'status': 200, 'message': 'ok', 'url': url})
