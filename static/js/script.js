946b58968e119a0f9f0d8dd3c697180490ea8487
FlipFlipAxe <sekirin.pavel@yandex.ru>
2015-10-23 16:39:25 +0200

    transfer script to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -42,4 +42,29 @@ $( "#reg_form" ).on('submit', function( event ) {
   }
 });
 
+$('.js-submitformat').click(function(){
+    document.forms['filterformats_form'].submit();
+});
+
+$('.js-submityear').click(function(){
+    getValue = $('#year').val();
+    if (!isYear(getValue)){
+        $(".errormsg").removeClass('hide');
+    }
+    else{
+        $(".errormsg").addClass('hide');
+        document.forms['filteryears_form'].submit();
+    }
+});
+
+function isYear(n) {
+    if(!isNaN(parseInt(n)) && isFinite(n)){
+        if (parseInt(n)>1900 && parseInt(n)<2100)
+            return true
+        else
+            return false;}
+    else
+        return false;
+} 
+
 /*************** YMAPS *****************/
