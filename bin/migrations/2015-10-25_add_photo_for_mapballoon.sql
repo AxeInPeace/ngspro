a2743abdd4e81900e8c7fdd2b4edb757131f1424
ruslan.galimov <ruslan.galimov@corp.mail.ru>
2015-10-25 23:33:11 +0100

    Fix migration

--- bin/migrations/2015-10-25_add_photo_for_mapballoon.sql
+++ bin/migrations/2015-10-25_add_photo_for_mapballoon.sql
@@ -1,3 +1,3 @@
-ALTER TABLE mapbaloon_balloon ADD COLUMN "material_photo_id" integer NOT NULL REFERENCES "photo_photo" ("id") DEFERRABLE INITIALLY DEFERRED DEFAULT 37;
+ALTER TABLE mapbaloon_balloon ADD COLUMN "material_photo_id" integer REFERENCES "photo_photo" ("id") DEFERRABLE INITIALLY DEFERRED;
 
 CREATE INDEX "mapbaloon_balloon_material_photo_id" ON "mapbaloon_balloon" ("material_photo_id");
