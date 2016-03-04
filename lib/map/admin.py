4ea25869341685a6588dd3217d73d4c00b99d923
ruslan.galimov <ruslan.galimov@corp.mail.ru>
2016-03-04 16:23:34 +0300

    Fix

--- lib/map/admin.py
+++ lib/map/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-
 from .models import Balloon, Polygon, Format, Instrument, TriangulationStation
-from django.contrib import admin
 
 
 class BalloonAdmin(admin.ModelAdmin):
     list_display = ('title', 'lng', 'lat', 'date', 'is_published')
     list_filter = ('is_published',)
 
+
 class TrgAdmin(admin.ModelAdmin):
     list_display = ('title', 'lng', 'lat', 'date', 'is_published')
     list_filter = ('is_published',)
+
 
 admin.site.register(Balloon, BalloonAdmin)
 admin.site.register(Polygon)
